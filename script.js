5a428cf0c259523b10f729a8a28fab620fd0e713
Gaberinoo <gabrielokao@hotmail.com>
2023-09-24 17:14:17 -0300

    update the algorithm and add style
    Finished the algorithm and added some style to the page.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const options = ['rock', 'paper', 'scissors'];
 
-const container = document.getElementById('container')
-const div = document.querySelector('#button-container')
+const container = document.getElementById('container');
+const div = document.querySelector('#button-container');
 const buttons = document.querySelectorAll('button');
 
 const para = document.createElement('p');
-const nextRoundButton = document.createElement('button')
+const nextRoundButton = document.createElement('button');
+const newGameButton = document.createElement('button');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -15,27 +16,29 @@ function gameStart(e) {
   let computerChoice = options[random];
   container.removeChild(div);
   if (e.target.id === computerChoice) {
-    container.appendChild(para).textContent = `its a draw`;
-    container.appendChild(nextRoundButton).textContent = 'play again';
+    container.appendChild(para).textContent = `I'ts a draw`;
+    container.appendChild(nextRoundButton).textContent = 'Next Round';
   } else if ((e.target.id === 'rock' && computerChoice === 'scissors') || 
   (e.target.id === 'paper' && computerChoice === 'rock') || 
   (e.target.id === 'scissors' && computerChoice === 'paper')) {
     playerScore++;
-    container.appendChild(para).textContent = `you win, your score is ${playerScore}`;
-    container.appendChild(nextRoundButton).textContent = 'play again';
+    container.appendChild(para).textContent = `You win, your score is ${playerScore}`;
+    container.appendChild(nextRoundButton).textContent = 'Next Round';
     if (playerScore === 5) {
-      container.appendChild(para).textContent = `match over, your score: ${playerScore} | computer score: ${computerScore}`;
-      container.appendChild(nextRoundButton);
+      container.removeChild(nextRoundButton);
+      container.appendChild(para).textContent = `Match Over! You won!`;
+      container.appendChild(newGameButton).textContent = "Play again";
     }
   } else if ((computerChoice === 'rock' && e.target.id === 'scissors') || 
   (computerChoice === 'paper' && e.target.id === 'rock') || 
   (computerChoice === 'scissors' && e.target.id === 'paper')) {
     computerScore++;
-    container.appendChild(para).textContent = `you lose, computer score is ${computerScore}`;
-    container.appendChild(nextRoundButton).textContent = 'play again';
+    container.appendChild(para).textContent = `You lose, opponent's score is ${computerScore}`;
+    container.appendChild(nextRoundButton).textContent = 'Next Round';
     if (computerScore === 5) {
-      container.appendChild(para).textContent = `match over, your score: ${playerScore} | computer score: ${computerScore}`;
-      container.appendChild(nextRoundButton);
+      container.removeChild(nextRoundButton);
+      container.appendChild(para).textContent = `Match Over... You lose.`;
+      container.appendChild(newGameButton).textContent = "Play again";
     }
   }
 }
@@ -45,5 +48,13 @@ buttons.forEach(item => item.addEventListener('click', gameStart));
 nextRoundButton.addEventListener('click', () => {
   container.removeChild(para);
   container.removeChild(nextRoundButton);
+  container.appendChild(div);
+})
+
+newGameButton.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  container.removeChild(para);
+  container.removeChild(newGameButton);
   container.appendChild(div);
 })
